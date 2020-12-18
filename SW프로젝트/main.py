990040a8cadbee0be17677311a1fd8736bfcb709
ab8454 <34102064+ab8454@users.noreply.github.com>
2020-12-18 17:15:22 +0900

    DOCS : week12 pull 후 재작성 / week16 오류

--- SW프로젝트/main.py
+++ SW프로젝트/main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.Qt import *
+from PyQt5 import Qt, QtCore
 from PyQt5 import uic
 from PyQt5 import *
 from PyQt5.QtWidgets import QApplication
@@ -8,8 +9,10 @@ from datetime import datetime
 from oracleDB import *
 from find import findWindow
 
+
 # notepad 기본, ui 참조 : https://onedrive.live.com/?cid=eb4f2a403d81809b&id=EB4F2A403D81809B%21201640&authkey=!AGYeXcxeLR6zXQU
 form_class = uic.loadUiType("C:\\Users\\ab845\\OneDrive - 인하공업전문대학\\swproject-notepad\\SW프로젝트\\notepad.ui")[0]
+
 
 class selectSubject(QDialog):
     sub_name = ""
@@ -17,7 +20,6 @@ class selectSubject(QDialog):
     sub_week = 0
     sub_date = now.strftime('%Y-%m-%d %H:%M:%S')
     sub_content = ""
-    sub_image=""
 
     def __init__(self):
         super(selectSubject, self).__init__()
@@ -25,8 +27,8 @@ class selectSubject(QDialog):
         self.setWindowTitle("과목 선택")
         self.show()
 
-        #db에서 subject 불러온 후 리스트에 저장
         sub_list = get_subject()
+
         for i in sub_list:
             self.list_subject.addItem(str(i))
 
@@ -38,10 +40,10 @@ class selectSubject(QDialog):
         self.btn_add.clicked.connect(self.addListWidget)
 
     def itemDoubleClicked(self):
-        try :
-            #과목 & week 둘다 선택 시
-            if(len(self.list_subject.currentItem().text()) >0):
-                if(int(self.list_week.currentItem().text()) > 0):
+        try:
+            # 과목 & week 둘다 선택 시
+            if (len(self.list_subject.currentItem().text()) > 0):
+                if (int(self.list_week.currentItem().text()) > 0):
                     selectSubject.sub_name = str(self.list_subject.currentItem().text())
                     selectSubject.sub_week = int(self.list_week.currentItem().text())
 
@@ -56,6 +58,7 @@ class selectSubject(QDialog):
         self.list_subject.addItem(self.sub_name)
         self.txt_addItem.setText("")
 
+
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
@@ -63,7 +66,7 @@ class WindowClass(QMainWindow, form_class):
 
         self.action_save.triggered.connect(self.save_file)
         self.action_close.triggered.connect(self.close)
-        self.action_open.triggered.connect(self.open)
+
         self.action_undo.triggered.connect(self.undoFunction)
         self.action_cut.triggered.connect(self.cutFunction)
         self.action_copy.triggered.connect(self.copyFunction)
@@ -78,35 +81,28 @@ class WindowClass(QMainWindow, form_class):
         self.action_unhide.triggered.connect(self.unhideFunction)
         self.action_capture.triggered.connect(self.captureFunction)
 
-        self.action_stt.triggered.connect(self.sttFunction)
-        
         data = self.textEdit.createMimeDataFromSelection()
         self.textEdit.canInsertFromMimeData(data)
-        #시작시 과목 선택s
+        # 시작시 과목 선택
         select = selectSubject()
         select.exec_()
-        
-        #내용 불러오기
+
+        # 내용 불러오기
         content = get_content(selectSubject.sub_name, selectSubject.sub_week, selectSubject.sub_date)
         self.setWindowTitle("과목 : " + selectSubject.sub_name)
         self.textEdit.setText(content)
 
-        #생성된 노트 마지막에 커서 위치
-        #self.cursor = self.plainTextEdit.textCursor()
-        #self.cursor.setPosition(len(content))
+        # 생성된 노트 마지막에 커서 위치
+        # self.cursor = self.plainTextEdit.textCursor()
+        # self.cursor.setPosition(len(content))
+
+        self.opened = False
 
         self.capture_cnt = 0
 
         self.setMouseTracking(True)
 
-    def sttFunction(self):
-        try:
-            record()
-        except Exception as ex:  # 에러 종류
-            print('에러가 발생 했습니다', ex)
-
-
-    #변경된 데이터 저장
+    # 변경된 데이터 저장
     def save_changed_data(self):
         msgBox = QMessageBox()
         msgBox.setWindowTitle('저장')
@@ -116,6 +112,7 @@ class WindowClass(QMainWindow, form_class):
         msgBox.addButton('취소', QMessageBox.RejectRole)  # 2
 
         # 최상단 고정
+        # msgBox.setWindowFlags(Qt.WindowStaysOnTopHint)
         msgBox.show()
 
         # 버튼 클릭 결과
@@ -126,7 +123,10 @@ class WindowClass(QMainWindow, form_class):
             return ret
 
     def closeEvent(self, event):
-        ret = self.save_changed_data()
+        if not self.saved:
+            ret = self.save_changed_data()
+
+        self.saved=False;
 
         if ret == 2:
             event.ignore()
@@ -137,26 +137,26 @@ class WindowClass(QMainWindow, form_class):
         sub_date = selectSubject.sub_date
 
         data = self.textEdit.toPlainText()
-
-        #선택한 주차 내용 불러오기
+        data=data.ljust(10)
+        # 선택한 주차 내용 불러오기
         content_start = data.find(sub_date) + len(sub_date) + 1
         sub_content = data[content_start:]
 
-        tup = (sub_name, sub_week, sub_date,sub_content)
+        tup = (sub_name, sub_week, sub_date, sub_content)
         add_subject(tup)  # oracle db에 저장
 
-    def open(self):
-        self.save_file()
-
-        now = datetime.now()
-        selectSubject.sub_date = now.strftime('%Y-%m-%d %H:%M:%S')
-
-        select = selectSubject()
-        select.exec_()
-
-        content = get_content(selectSubject.sub_name, selectSubject.sub_week, selectSubject.sub_date)
-        self.setWindowTitle("과목 : " + selectSubject.sub_name)
-        self.textEdit.setText(content)
+        self.saved = True
+        '''
+        # data 마지막시간 기준으로 그 후 데이터 찾기
+        compare_string = "==================="
+        iCnt=1 
+        # 주차 별 내용 담기
+        for date in date_list:
+            content_start = data.find(date) +len(date) + 1
+            content_end = data.find(compare_string,iCnt+1) -1
+            selectSubject.sub_content= data[content_start:content_end]
+            print(selectSubject.sub_content)
+        '''
 
     def undoFunction(self):
         self.plainTextEdit.undo()
@@ -198,7 +198,6 @@ class WindowClass(QMainWindow, form_class):
 
         pyautogui.screenshot('D:/{}.png'.format(self.now), region=(500, 100, 1000, 700))
         print("캡쳐 완료")
-
 
 
 app = QApplication(sys.argv)
--- SW프로젝트/oracleDB.py
+++ SW프로젝트/oracleDB.py
@@ -6,18 +6,16 @@ date_list = []  # date_name 리스트
 week_list = []  # week_name 리스트
 content_list = []  # content_name 리스트
 
-#시작시 과목 출력 함수
 def get_subject():
     os.putenv('NLS_LANG', '.UTF8')
-    con1 = cx_Oracle.connect('SYSTEM/AB8488454@localhost:1521/ORCL')
+    #con1 = cx_Oracle.connect('SYSTEM/AB8488454@localhost:1521/ORCL')
+    con1 = cx_Oracle.connect('SYSTEM/AB8488454@192.168.56.1:1521/ORCL')
     cursor = con1.cursor()
 
     #program 시작시 과목 출력 db
     cursor.execute('select sub_name from notepad')
-
     # sub_name 리스트
     sub_list =[]
-
     #과목 리스트 db에서 가져옴
     for row in cursor:
         if str(row).strip('()/\',') not in sub_list: # 중복 제거
@@ -27,7 +25,6 @@ def get_subject():
     con1.close()
     return sub_list
 
-#선택한 과목 내용 출력
 def get_content(sub_name, sub_week,sub_date):
     os.putenv('NLS_LANG', '.UTF8')
     con1 = cx_Oracle.connect('SYSTEM/AB8488454@localhost:1521/ORCL')
@@ -38,16 +35,18 @@ def get_content(sub_name, sub_week,sub_date):
 
     try:
         for row in cursor:
-            if( row[0] == sub_name): #선택한 과목명과 db의 과목명이 같을 경우
-
-                if(row[1] == sub_week): #선택한 과목명과 주차가 이미 존재할 시
+            if( row[0] == sub_name): 
+            #선택한 과목명과 db의 과목명이 같을 경우
+            
+                if(row[1] == sub_week): 
+                #선택한 과목명과 주차가 이미 존재할 시
                     is_exist = True
 
-                else:
-                    week_list.append(row[1])
-                    date_list.append(row[2])
-                    content_list.append(row[3])
+                week_list.append(row[1])
+                date_list.append(row[2])
+                content_list.append(row[3])
 
+        #내용 담을 변수
         content_notepad= ""
 
         for i in range(len(week_list)):
@@ -69,20 +68,17 @@ def get_content(sub_name, sub_week,sub_date):
 
     return content_notepad
 
-#저장 후 db에 추가/수정
 def add_subject(tup):
     os.putenv('NLS_LANG', '.UTF8')
     con1 = cx_Oracle.connect('SYSTEM/AB8488454@localhost:1521/ORCL')
     cursor = con1.cursor()
-
     cursor.execute("SELECT * FROM notepad order by sub_week ")
     try:
         for row in cursor:
             #과목명이 같은 경우
             if( row[0] == tup[0]):
-
                 #과목명 같고 주차 같은경우
-                if (row[1]==tup[1]):  # sub_content 저장 후 다시 저장 할때
+                if (row[1] == tup[1]):  # sub_content 저장 후 다시 저장 할때
                     query = "UPDATE NOTEPAD SET sub_content =(:4) " \
                             "where sub_name = (:1) and sub_week = (:2)"
                     cursor.execute(query,tup)
@@ -100,7 +96,7 @@ def add_subject(tup):
                     con1.close()
                     return
 
-        #과목명 다른 경우 / db가 없을 경우
+        #과목명 다른 경우 row는 x tup에만 있음
         query = "INSERT INTO NOTEPAD VALUES (:1,:2,:3,:4)"
         cursor.execute(query, tup)
         con1.commit()
@@ -108,7 +104,5 @@ def add_subject(tup):
     except Exception as ex:  # 에러 종류
         print('에러가 발생 했습니다', ex)
 
-
     cursor.close()
     con1.close()
-
